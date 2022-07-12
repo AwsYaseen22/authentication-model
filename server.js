96754687038bf7cfa99fbb9304fdfc772bd5db4c
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-12 17:11:13 +0300

    test the basic registration and connectio of the server and the DB

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT;
 const connectDB = require("./db");
 
 connectDB();
 
+console.log(PORT);
 app.use(express.json());
 app.use("/api/Auth", require("./Auth/Route"));
 
 const server = app.listen(PORT, () => {
-  console.log("Connected to the sever successfully!");
+  console.log(`Connected to the sever successfully! port:${PORT}`);
 });
 
 process.on("unhandledRejection", (err) => {
